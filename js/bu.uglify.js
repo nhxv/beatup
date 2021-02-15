859b0954a7dd33728f9e7116579a334752b43807
nhxv <ngohoangxuanvinh1995@gmail.com>
2021-02-14 18:25:12 -0800

    change blue beatup ratio

--- js/bu.uglify.js
+++ js/bu.uglify.js
@@ -1007,8 +1007,8 @@ BUJS.Game_ = function (songId) {
 
     this.noteScores_ = [520, 260, 130, 26, 0];
     this.spaceScores_ = [2000, 1500, 1000, 500, 0];
-    this.yellowBeatupRatio_ = 1.25;
-    this.blueBeatupRatio_ = 1.5;
+    this.yellowBeatupRatio_ = 1.2;
+    this.blueBeatupRatio_ = 1.55;
 
 
     // load music and renderer
