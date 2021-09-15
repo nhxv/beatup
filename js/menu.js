0a7c6cda49737f5c0a64db8abc48dcb5d0eb860d
nhxv <ngohoangxuanvinh1995@gmail.com>
2021-09-15 14:13:26 -0700

    update menu navigation after select

--- js/menu.js
+++ js/menu.js
@@ -54,8 +54,21 @@ export class Menu {
     }
 
     chooseSong(id) {
-        // TODO: navigation maybe
+        // TODO: hide modal
         new Game(id, songList);
+    }
+
+    closeModal(modalId) {
+        // get modal
+        const modal = document.getElementById(modalId);
+        // change state like in hidden modal
+        modal.classList.remove('show');
+        modal.setAttribute('aria-hidden', 'true');
+        modal.setAttribute('style', 'display: none');
+        // get modal backdrop
+        const modalBackdrops = document.getElementsByClassName('modal-backdrop');
+        // remove opened modal backdrop
+        document.body.removeChild(modalBackdrops[0]);
     }
 
     showLoadingMsg() {
